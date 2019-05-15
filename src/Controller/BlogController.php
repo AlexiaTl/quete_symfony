6213f50cdb943c103cec16fb8496b1a585681ae1
Alexia Tali <alexiatali@mbp-de-alexia.home>
2019-05-15 16:02:07 +0200

    Symfony - quête 4

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,19 +4,38 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+/**
+ * @Route("/blog", name="blog_")
+ */
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog_index")
+     * @Route("/", name="index")
     */
     public function index()
     {
         return $this->render('blog/index.html.twig', [
             'owner' => 'Alexia',
         ]);
+    }
+
+
+    /**
+     * @Route("/list/{page<\d+>?1}", name="list")
+     */
+    public function list($page)
+    {
+        return $this->render('blog/list.html.twig', ['page' => $page]);
+    }
+
+
+    /**
+     * @Route("/show/{slug<^[a-z0-9-]+$>}", defaults={"slug"="Article Sans Titre"}, name="show")
+     */
+    public function show($slug){
+        $slug = preg_replace("/-/"," ", ucwords(trim(strip_tags($slug)), "-"));
+        return $this->render('blog/show.html.twig', ['slug' => $slug]);
     }
 }
