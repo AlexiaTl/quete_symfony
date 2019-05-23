e91448f6cdcc3d3149bed27fca99f5276103e3f4
Alexia Tali <alexiatali@mbp-de-alexia.home>
2019-05-23 09:40:48 +0200

    Quête 7

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,11 +1,10 @@
 <?php
-// src/Controller/BlogController.php
 namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
 use App\Entity\Category;
 
@@ -15,7 +14,7 @@ class BlogController extends AbstractController
     /**
      * Show all row from article's entity
      *
-     * @Route("/", name="index")
+     * @Route("/blog/show", name="index")
      * @return Response A response instance
      */
     public function index(): Response
@@ -23,64 +22,71 @@ class BlogController extends AbstractController
         $articles = $this->getDoctrine()
             ->getRepository(Article::class)
             ->findAll();
-
         if (!$articles) {
             throw $this->createNotFoundException(
-            'No article found in article\'s table.'
+                'No article found in article\'s table.'
             );
         }
-
         return $this->render(
-                'blog/index.html.twig',
-                ['articles' => $articles]
+            'blog/show.html.twig',
+            ['articles' => $articles]
         );
     }
 
+
     /**
-     * @Route("/blog/show/", name="blog_list")
+     * Getting a article with a formatted slug for title
+     *
+     * @param string $slug The slugger
+     *
+     * @Route("/blog/show/{slug<^[a-z0-9-]+$>}",
+     *     defaults={"slug" = null},
+     *     name="blog_show")
+     *  @return Response A response instance
      */
-    public function Default()
+    public function show(?string $slug) : Response
     {
-        return $this->render('blog/list.html.twig');
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
+        }
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'No article with '.$slug.' title, found in article\'s table.'
+            );
+        }
+        return $this->render(
+            'blog/show.html.twig',
+            [
+                'article' => $article,
+                'slug' => $slug,
+            ]
+        );
     }
 
-    /**
-     * @Route("/blog/show/{slug<^[a-z0-9-]+$>?Article Sans Titre}", name="blog_show")
-     */
-    // redirection vers la page blog show
-    public function show($slug){
-        $slug = ucwords(preg_replace("/-/"," ",$slug));
-        return $this->render('blog/show.html.twig', ['slug' => $slug]);
-    }
-
 
     /**
-     * @param string $categoryName
-     * @return Response
-     * @Route("/blog/showcategory/{categoryName}", name="show_category")
+     ** @Route("/blog/category/{category}", name="show_category")
      */
-    public function showByCategory(string $categoryName)
+    public function showByCategory(string $category) :Response
     {
-        $category = $this->getDoctrine()->getRepository(Category::class)
-            ->findOneBy(['name'=>$categoryName]);
-        $articles = $this->getDoctrine()->getRepository(Article::class)
-            ->findBy(['category'=>$category->getId()], ['id'=>'DESC'], 3);
-        return $this->render('blog/category.html.twig', ['articles' => $articles, 'category' => $categoryName]);
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name' => $category]);
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findBy(['category' => $category],[ "id" =>"desc"],3);
+        return $this->render(
+            'blog/category.html.twig',
+            ['articles' => $articles,
+                'category' => $category,
+            ]);
     }
-
-    
-    /**
-     * @Route("/blog/error/{slug}",
-     * requirements={"slug"="[A-Z]+"},
-     * defaults={"slug"="UPPER-CASE-NOT-ALLOWED"},
-     * name="error")
-     */
-    public function error($slug)
-    {
-        // redirection vers la page erreur, correspondant à l'insertion de majuscule dans l'URL
-        return $this->render('blog/error_404.html.twig', ['slug' => $slug]);
-    }
-
-
-
 }
